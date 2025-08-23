cdd04b46c2d944a8dcd913aed953eb7b62149cb6
Nidhi Srivastav <nidhisrivastav283@gmail.com>
2025-08-23 10:05:32 +0530

    New commit

--- src/main/java/com/hangouthub/hangouthub/services/MoodServiceImpl.java
+++ src/main/java/com/hangouthub/hangouthub/services/MoodServiceImpl.java
@@ -1,10 +1,8 @@
 package com.hangouthub.hangouthub.services;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.hangouthub.hangouthub.models.Mood;
 import com.hangouthub.hangouthub.repository.MoodRepo;
 
@@ -17,17 +15,14 @@ public class MoodServiceImpl implements MoodService{
 	public List<Mood> getAllMoods() {
 		return moodRepo.findAll();
 	}
-	
 	@Override
 	public Mood getMoodById(Long id) {
 		return moodRepo.findById(id).orElse(null);
 	}
-
 	@Override
 	public Mood saveMood(Mood mood) {
 		return moodRepo.save(mood);
 	}
-
 	@Override
 	public void deleteMood(Long id) {
 		moodRepo.deleteById(id);
