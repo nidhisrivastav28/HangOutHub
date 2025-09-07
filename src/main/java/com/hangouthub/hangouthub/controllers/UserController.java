c7e175ea845227e3574f4a417ef04e80bf9423e1
Nidhi Srivastav <nidhisrivastav283@gmail.com>
2025-09-07 12:49:48 +0530

    new updates

--- src/main/java/com/hangouthub/hangouthub/controllers/UserController.java
+++ src/main/java/com/hangouthub/hangouthub/controllers/UserController.java
@@ -68,6 +68,6 @@ public class UserController {
     @GetMapping("/logout")
     public String logout(HttpSession session){
         session.invalidate();
-        return "redirect:/login";
+        return "redirect:/home";
     }
 }
